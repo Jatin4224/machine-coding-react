7f1da0cb9adae7c511f1f80c0013ffc2b8d87b63
Jatin4224 <jaikumarsharma94130@gmail.com>
2025-02-04 11:35:28 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import TabForm from "./components/TabForm";
 
 const App = () => {
--- src/components/TabForm.jsx
+++ src/components/TabForm.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useState } from "react";
+import Interests from "./Interests";
+import Profile from "./Profile";
+import Settings from "./Settings";
 
 const TabForm = () => {
-  return <div>TabForm</div>;
+  const [activeTab, setActiveTab] = useState(0);
+  const tabs = [
+    {
+      name: "Profile",
+      component: Profile,
+    },
+    {
+      name: "Interests",
+      component: Interests,
+    },
+    {
+      name: "Settings",
+      component: Settings,
+    },
+  ];
+
+  const ActiveTabComponent = tabs[activeTab].component;
+  return (
+    <div>
+      <div className="heading-container">
+        {tabs.map((t, index) => (
+          <div
+            className={`heading ${activeTab === index ? "active" : ""}`}
+            onClick={() => setActiveTab(index)}
+            key={index}
+          >
+            {t.name}
+          </div>
+        ))}
+      </div>
+      <div className="tab-body">
+        <ActiveTabComponent />
+      </div>
+    </div>
+  );
 };
 
 export default TabForm;
